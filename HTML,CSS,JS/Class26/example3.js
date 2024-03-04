4f749d0c08cc2169173aa9d0911e06ffcf3d656c
Vaibhav Zade <vzade1999@gmail.com>
2024-03-04 21:08:29 +0530

    final

---
+++ HTML,CSS,JS/Class26/example3.js
@@ -0,0 +1,26 @@
+// Topic 1:
+
+// Falsy Value = 0 null undefined '' false NaN
+
+let a = 12;
+
+if (a) {
+    console.log('Truthy Value');
+} else {
+    console.log('Falsy Value');
+}
+
+
+// Topic 2:
+
+let b = 1;
+let c = '1';
+
+console.log(1 == '1');
+console.log(1 === '1');
+
+
+typeof X //
+
+Array.isArray() //
+isNaN() // 
